c8970ed33b455e2f36a205a1885354dc973bdc9a
DarshanAware <144710362+blaze-d83@users.noreply.github.com>
2024-10-08 10:42:42 +0530

    updated logger: returns method

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -13,17 +13,20 @@ func GetCustomLoggerConfig(e *echo.Echo) *middleware.RequestLoggerConfig {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	return &middleware.RequestLoggerConfig{
 		LogStatus:   true,
+		LogMethod:   true,
 		LogURI:      true,
 		LogError:    true,
 		HandleError: true,
 		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
 			if v.Error == nil {
 				logger.LogAttrs(context.Background(), slog.LevelInfo, "REQUEST",
+					slog.String("method", v.Method),
 					slog.String("uri", v.URI),
 					slog.Int("status", v.Status),
 				)
 			} else {
 				logger.LogAttrs(context.Background(), slog.LevelError, "REQUEST ERROR",
+					slog.String("method", v.Method),
 					slog.String("uri", v.URI),
 					slog.Int("status", v.Status),
 					slog.String("err", v.Error.Error()),
