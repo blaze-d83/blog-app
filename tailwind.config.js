61c15e3c9966de4f62e186b6631274a5598b51a1
DarshanAware <144710362+blaze-d83@users.noreply.github.com>
2024-10-09 21:02:52 +0530

    ✔  static file serving problem solved!
    - static file serving inside router points to correct address

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-        "./internal/templates/*.{html, js, templ, go}",
-        "./internal/templates/components/*.{html, js, templ, go}",
+        "./internal/templates/**/*.templ",
+        "./internal/templates/components/**/*.templ",
     ],
   theme: {
     extend: {
